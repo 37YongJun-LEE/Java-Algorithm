abf22c67495e0d95efe726eabb6b3afe044a5478
이용준 <108706790+37YongJun-LEE@users.noreply.github.com>
2025-05-09 15:24:55 +0900

    [level 1] Title: [PCCP 기출문제] 1번 / 붕대 감기, Time: 0.02 ms, Memory: 87.2 MB -BaekjoonHub

--- Java/프로그래머스/1/250137. ［PCCP 기출문제］ 1번 ／ 붕대 감기/［PCCP 기출문제］ 1번 ／ 붕대 감기.java
+++ Java/프로그래머스/1/250137. ［PCCP 기출문제］ 1번 ／ 붕대 감기/［PCCP 기출문제］ 1번 ／ 붕대 감기.java
@@ -11,27 +11,17 @@ class Solution {
         int time = 0;
         
         for (int[] attack : attacks) {
-            System.out.println(time);
-            System.out.println(Arrays.toString(attack));
             int gap = attack[0] - time - 1;
-            
             time = attack[0];
             int deal = attack[1];
             
-            System.out.println(gap);
-            
-            // 회복
             hp += gap * hps ; // 초당 회복
             hp += (gap / t) * tps; // t초당 회복
             if (hp >= health) hp = health;
 
-            // 공격
             hp -= deal;
             if (hp <= 0 ) return -1;
-            
-            // System.out.println("==================");
         }
-        
         return hp;
     }
 }
